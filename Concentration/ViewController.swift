630d0aec3bc833a7d9934c58d11acdfc72bb75d3
Bomi Kwon <bomikwon@wifi_stu-10-38-75-44.uncc.edu>
2018-05-20 17:36:13 -0400

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Bomi Kwon on 5/20/18.
+//  Copyright © 2018 Bomi Kwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
